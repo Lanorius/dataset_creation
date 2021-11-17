586100ad4d48f6da7576bcdbe11f7c0f3571a3d1
Lanorius <Danielsg@web.de>
2021-11-17 10:05:39 +0100

    whole raw transformation is now in the new file

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-import pandas as pd
-import ast  # to go from string to list while parsing col-names
-import math  # to calculate better chunk sizes
-import os  # to remove intermediate files
+# import pandas as pd
+# import ast  # to go from string to list while parsing col-names
+# import math  # to calculate better chunk sizes
 
-import traceback
+
+# import traceback
 from process_inputs import parse_config
 from src.functions import *
-import numpy as np
-import subprocess  # to run CD-Hit and mayachemtools
+# import numpy as np
+# import subprocess  # to run CD-Hit and mayachemtools
 
-from tqdm import tqdm  # shows progress of for loops
+# from tqdm import tqdm  # shows progress of for loops
 
 # import matplotlib.pyplot as plt  # for the boxplots
 
@@ -37,81 +37,12 @@ tasks_to_perform, files, file_specifications, output, params = parse_config()
 if not os.path.isdir(files['path']):
     os.mkdir(files['path'])
 
-# files is writen as plural, however for now it only works with a single file
-# params will very likely be moved to another file
-
-raw_transformer(files, file_specifications, output)
-
-'''
+# PART 1 Raw Transformation
 if tasks_to_perform[0]:
-    print("Part 1: Performing raw transformation.")
+    print("Part 1: Performing raw transformation and creating drug, target, and interaction files.")
+    raw_transformer(files, file_specifications, output)
+    create_raw_files(files, file_specifications, output)
 else:
-    print("Part 1: Skipping raw transformation.")
+    print("Part 1: Skipping raw transformation, and the creating of drug, target, and interaction files.")
 
-if tasks_to_perform[0]:
-    path = files['bindingDB_file']
-    sep = file_specifications['separator_one']
-    cols = [file_specifications['protein_IDs'], file_specifications['ligand_IDs'],
-            file_specifications['protein_sequence'], file_specifications['ligand_SMILE'],
-            file_specifications['interaction_value']]
-    # cols = ast.literal_eval(file_specifications['colnames_one'])
-
-    cleaned_frame = pd.DataFrame(columns=cols)
-
-    chunksize = math.ceil(len(list(open(path))) / 5)  # allows handeling large sets of input data
-    for chunk in pd.read_csv(filepath_or_buffer=path, sep=sep, chunksize=chunksize, usecols=cols, on_bad_lines='skip',
-                             engine='python'):
-        # chunk = chunk[chunk['Target Source Organism According to Curator or DataSource'] == "Homo sapiens"]
-        # removed from the config for now, since we want a general understanding of binding
-        chunk = chunk.dropna(how='any', subset=[file_specifications['protein_IDs']])
-        chunk = chunk.dropna(how='any', subset=[file_specifications['ligand_IDs']])
-        chunk[file_specifications['interaction_value']] = pd.to_numeric(chunk[file_specifications['interaction_value']],
-                                                                        errors='coerce')
-        chunk = chunk.dropna(how='any', subset=[file_specifications['interaction_value']])
-        cleaned_frame = pd.concat([cleaned_frame, chunk])
-    # this needs to be adjusted to the config file
-    cleaned_frame.to_csv(cleaned_file, sep='\t')
-'''
-# PART 2
-# move the other file here
-
-print("Part 2: Creating necessary files.")
-
-path = output['cleaned_frame']
-sep = '\t'  # should come from config
-
-f = open(output['fasta_file'], 'w')
-d = open('temp_drugs.txt', 'w')
-
-file = pd.read_csv(filepath_or_buffer=path, sep=sep, engine='python')
-
-for index, row in file.iterrows():
-
-    f.write(">" + row[file_specifications['protein_IDs']] + "\n")
-    i = 0
-    while i < len(row[file_specifications['protein_sequence']]):
-        if i % 40 != 39:
-            f.write(row[file_specifications['protein_sequence']][i])
-            i += 1
-        else:
-            f.write(row[file_specifications['protein_sequence']][i])
-            f.write("\n")
-            i += 1
-    if i % 40 != 0:
-        f.write("\n")
-
-    d.write(row[file_specifications['ligand_SMILE']] + " " + row[file_specifications['ligand_IDs']] + "\n")
-
-# ensures the output is a csv file
-# all duplicate lines are removed here from the drugs as well
-temp_drugs = pd.read_csv('temp_drugs.txt', sep=' ').drop_duplicates(keep='first').reset_index()
-temp_drugs = temp_drugs.iloc[:, 1:]
-temp_drugs.to_csv(output['drug_file'], sep=' ', index=False)
-os.remove('temp_drugs.txt')
-
-interactions = file.pivot_table(index=file_specifications['ligand_IDs'], columns=file_specifications['protein_IDs'],
-                                values=file_specifications['interaction_value'], aggfunc='sum')
-interactions.to_csv(output['interaction_file'], sep='\t')
-
-f.close()
-d.close()
+# PART 2 Clustering
--- src/functions.py
+++ src/functions.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import math  # to calculate better chunk sizes
+import os  # to remove intermediate files
 
 
 def raw_transformer(raw_info, specifications, out):
-    sep = specifications['separator']
+    # takes a data_set, like the one from BindingDB and does a basic cleanup to it
     cols = [specifications['protein_IDs'], specifications['ligand_IDs'],
             specifications['protein_sequence'], specifications['ligand_SMILE'],
             specifications['interaction_value']]
@@ -11,8 +12,8 @@ def raw_transformer(raw_info, specifications, out):
     cleaned_frame = pd.DataFrame(columns=cols)
 
     chunksize = math.ceil(len(list(open(raw_info['raw_file']))) / 5)  # allows handeling large sets of input data
-    for chunk in pd.read_csv(filepath_or_buffer=raw_info['raw_file'], sep=sep, chunksize=chunksize, usecols=cols,
-                             on_bad_lines='skip', engine='python'):
+    for chunk in pd.read_csv(filepath_or_buffer=raw_info['raw_file'], sep=specifications['separator'],
+                             chunksize=chunksize, usecols=cols, on_bad_lines='skip', engine='python'):
         # chunk = chunk[chunk['Target Source Organism According to Curator or DataSource'] == "Homo sapiens"]
         # removed from the config for now, since we want a general understanding of binding
         chunk = chunk.dropna(how='any', subset=[specifications['protein_IDs']])
@@ -22,5 +23,44 @@ def raw_transformer(raw_info, specifications, out):
         chunk = chunk.dropna(how='any', subset=[specifications['interaction_value']])
         cleaned_frame = pd.concat([cleaned_frame, chunk])
     cleaned_frame.to_csv(raw_info['path']+out['cleaned_frame'], sep='\t')
+
+    return 0
+
+
+def create_raw_files(raw_info, specifications, out):
+    f = open(raw_info['path'] + out['fasta_file'], 'w')
+    d = open('temp_drugs.txt', 'w')
+    file = pd.read_csv(filepath_or_buffer=(raw_info['path'] + out['cleaned_frame']), sep='\t', engine='python')
+
+    for index, row in file.iterrows():
+
+        f.write(">" + row[specifications['protein_IDs']] + "\n")
+        i = 0
+        while i < len(row[specifications['protein_sequence']]):
+            if i % 40 != 39:
+                f.write(row[specifications['protein_sequence']][i])
+                i += 1
+            else:
+                f.write(row[specifications['protein_sequence']][i])
+                f.write("\n")
+                i += 1
+        if i % 40 != 0:
+            f.write("\n")
+
+        d.write(row[specifications['ligand_SMILE']] + " " + row[specifications['ligand_IDs']] + "\n")
+
+    # ensures the output is a csv file
+    # all duplicate lines are removed here from the drugs as well
+    temp_drugs = pd.read_csv('temp_drugs.txt', sep=' ').drop_duplicates(keep='first').reset_index()
+    temp_drugs = temp_drugs.iloc[:, 1:]
+    temp_drugs.to_csv(raw_info['path'] + out['drug_file'], sep=' ', index=False)
+    os.remove('temp_drugs.txt')
+
+    interactions = file.pivot_table(index=specifications['ligand_IDs'], columns=specifications['protein_IDs'],
+                                    values=specifications['interaction_value'], aggfunc='sum')
+    interactions.to_csv(raw_info['path'] + out['interaction_file'], sep='\t')
+
+    f.close()
+    d.close()
 
     return 0
